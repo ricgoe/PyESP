19d5f526a600b30846f0c57273f5379e03dd8467
ricgoe <bluemchen_fenchel_0d@icloud.com>
2023-12-15 12:29:34 +0100

    added mqtt base

---
+++ Dependencies/boot.py
@@ -0,0 +1,31 @@
+import time
+from umqttsimple import MQTTClient
+import ubinascii
+import machine
+import micropython
+import network
+import esp
+esp.osdebug(None)
+import gc
+gc.collect()
+
+ssid = 'REPLACE_WITH_YOUR_SSID'
+password = 'REPLACE_WITH_YOUR_PASSWORD'
+mqtt_server = 'REPLACE_WITH_YOUR_MQTT_BROKER_IP'
+client_id = ubinascii.hexlify(machine.unique_id())
+topic_pub = b'ESP32'
+
+last_message = 0
+message_interval = 5
+counter = 0
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
