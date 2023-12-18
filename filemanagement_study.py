1b35afb96b3a7db5fa7c911260f67371cf3d0e8a
ricgoe <bluemchen_fenchel_0d@icloud.com>
2023-12-18 19:22:12 +0100

    made study on parameters which influence measurements

---
+++ filemanagement_study.py
@@ -0,0 +1,13 @@
+
+import os
+import shutil
+
+def pull_log_from_mprem():
+    path = os.path.join(os.getcwd(), 'mprem_files')
+    path_to_push = os.path.join(os.getcwd(), 'Study')
+    files = os.listdir(path)
+    for file in files:
+        if file.endswith(".csv"):
+            shutil.move(os.path.join(path,file), path_to_push)
+
+pull_log_from_mprem() 
