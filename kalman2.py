04405e90fe284428b0111ec48a83ae6bad379f98
ricgoe <bluemchen_fenchel_0d@icloud.com>
2023-12-23 11:00:31 +0100

    changed kalman to 1 dim

--- kalman2.py
+++ kalman2.py
@@ -9,16 +9,26 @@ def dv_kalman(z):
     if firstRun is None:
         firstRun = 1
  
-        A = np.array([[1, 1],
-                      [0, 1]])
-        H = np.array([1, 0])
+        # A = np.array([[1, 1],
+        #               [0, 1]])
+        # H = np.array([1, 0])
 
-        Q = np.array([[1*(10**-5), 0],
-                      [0, 0.003]])
-        R = 2.92*(10**-3)
+        # Q = np.array([[1*(10**-5), 0],
+        #               [0, 0.003]])
+        # R = 2.92*(10**-3)
 
-        x = np.array([100, 0]).reshape(-1, 1)
-        P = 100 * np.eye(2)
+        # x = np.array([100, 0]).reshape(-1, 1)
+        # P = 100 * np.eye(2)
+        
+        
+        A = np.matrix([[1.]])
+        #B = np.matrix([[0.]])
+        H = np.matrix([[1.]])
+        Q = np.matrix([[0.00001]])
+        R = np.matrix([[2.92*(10**-3)]])
+        
+        x = np.matrix([[0.]])      # initial distance estimate
+        P = np.matrix([[1000.]])
 
     # Kalman filter algorithm
     xp = A * x
@@ -29,8 +39,10 @@ def dv_kalman(z):
     x = xp + K * (z - H * xp)
     P = Pp - K * H * Pp
 
-    pos = x[0, 0]
-    delta_d = x[1, 0]
+    #pos = x[0, 0]
+    delta_d = 0#x[1, 0]
+    pos = x
+    
     Px = P
 
     return pos, delta_d, Px
