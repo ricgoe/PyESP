c9d296f4097a9b08baf6ce099533a7c44a909501
ricgoe <bluemchen_fenchel_0d@icloud.com>
2023-12-25 20:15:17 +0100

    changed ip adress to broker adress

--- pahhhhhoni.py
+++ pahhhhhoni.py
@@ -5,7 +5,7 @@ import pandas as pd
 from io import StringIO
 
 # Configuration
-broker = "192.168.178.152"
+broker = "10.42.0.1"
 port = 1883
 
 def sender(**kwargs):
@@ -38,6 +38,7 @@ def listener():
 def on_message(client: mqtt.Client, userdata, msg):
     df = pd.read_csv(StringIO(msg.payload.decode('utf-8')))
     print(df)
+    df.to_csv("data_test.csv", index=False)
     fig, ax = plt.subplots(figsize = (16, 8))
     ax.plot(df.columns[:-1], df.iloc[0, :-1])
     ax.set_ylim(0, 250)
@@ -47,7 +48,7 @@ def on_message(client: mqtt.Client, userdata, msg):
     client.disconnect()
 
 if __name__ == '__main__':
-    sender(password="jimmy4", angle=45, stop=False, runs=1, shape="triangle")
+    sender(password="jimmy4", angle=2, stop=False, runs=1, shape="triangle")
     listener()
     
     
