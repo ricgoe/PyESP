4b995a706ad85a2a413201555124546e78904b7e
ricgoe <bluemchen_fenchel_0d@icloud.com>
2024-01-04 18:37:52 +0100

    changed things...

--- pahhhhhoni.py
+++ pahhhhhoni.py
@@ -39,16 +39,27 @@ def on_message(client: mqtt.Client, userdata, msg):
     df = pd.read_csv(StringIO(msg.payload.decode('utf-8')))
     print(df)
     df.to_csv("data_test.csv", index=False)
+    # fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+    #ax.plot(df.columns[:-1], df.iloc[0, :-1])
+    # ax.plot(df.columns[:-1], df.iloc[0, :-1], 'o-', label='Messreihe 1') # Linie mit Punkten
+    #ax.set_ylim(0, 250)
+    #ax.set_xticks([x for x in range(0, len(df.columns), 8)])
+    #ax.set_rmax(12) # Maximaler Radius
+    # ax.set_rticks([4, 6, 8, 10]) # Radien-Markierungen
+    # ax.set_rlabel_position(-22.5) # Position der Radien-Beschriftung
+    # ax.grid(True)
+    # ax.legend(loc='upper right') # Legende
+    # plt.show()
     fig, ax = plt.subplots(figsize = (16, 8))
     ax.plot(df.columns[:-1], df.iloc[0, :-1])
-    ax.set_ylim(0, 250)
+    ax.set_ylim(75, 200)
     ax.set_xticks([x for x in range(0,len(df.columns), 8)])
     plt.show()
     client.loop_stop()
     client.disconnect()
 
 if __name__ == '__main__':
-    sender(password="jimmy4", angle=2, stop=False, runs=1, shape="triangle")
+    sender(password="jimmy4", angle=5, stop=False, runs=1, shape="triangle", mode = "angle")
     listener()
     
     
