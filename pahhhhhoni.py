972475d00b35bd189b21ba7cc4f44d1c2939de90
ricgoe <bluemchen_fenchel_0d@icloud.com>
2024-01-11 20:49:58 +0100

    misc

--- pahhhhhoni.py
+++ pahhhhhoni.py
@@ -43,7 +43,8 @@ def on_message(client: mqtt.Client, userdata, msg):
     output_filename=f'scans_angle_{str(df.iloc[-1,-1]).replace('.','_')}_pos_{str(df.iloc[-1,-3])}_rot_{str(df.iloc[-1,-2])}.csv'
     output_path = os.path.join(os.getcwd(), 'Logs')
     output_filepath = os.path.join(output_path, output_filename)
-    df.to_csv(output_filepath, mode='a', header=not os.path.exists(output_filepath), index=False)
+    #df.to_csv(output_filepath, mode='a', header=not os.path.exists(output_filepath), index=False)
+    df.to_csv('test.csv', index=False)
     # fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
     #ax.plot(df.columns[:-1], df.iloc[0, :-1])
     # ax.plot(df.columns[:-1], df.iloc[0, :-1], 'o-', label='Messreihe 1') # Linie mit Punkten
@@ -57,15 +58,15 @@ def on_message(client: mqtt.Client, userdata, msg):
     # plt.show()
     fig, ax = plt.subplots(figsize = (16, 8))
     ax.plot(df.columns[:-4], df.iloc[0, :-4])
-    ax.set_ylim(75, 200)
+    #ax.set_ylim(75, 200)
     ax.set_xticks([x for x in range(0,len(df.columns), 8)])
-    #plt.show()
+    plt.show()
     client.loop_stop()
     client.disconnect()
 
 if __name__ == '__main__':
     # sensor_rotation = 1 -> sensor is rotated vertically, sensor_rotation = 0 -> sensor is rotated horizontally
-    sender(password="jimmy4", angle=11.25, stop=False, runs=1, shape="triangle", mode = "angle", position = 8, sensor_rotation = 1)
+    sender(password="jimmy4", angle=11.25, stop=True, runs=2, shape="triangle", mode = "angle", position = 8, sensor_rotation = 1)
     listener()
     
 
