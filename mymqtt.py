0a669aa5ef966cc578b92c20aa7a578ca81bf6a0
YolloPlays <spexe0011@gmail.com>
2024-01-19 09:14:36 +0100

    18.01 Programmsitzung delay removal

--- mymqtt.py
+++ mymqtt.py
@@ -17,6 +17,19 @@ my_stepper = Stepper(19, 18, 5, 17)
 
 FULL_ROTATION = Stepper.FULL_ROTATION
 
+def publish(datastr):
+    client = MQTTClient("ESP32DATA", SERVER, PORT)
+    try:
+        client.connect()
+        client.publish(b"data", datastr)
+        print(datastr)
+    except Exception as e:
+        print("Error1: %s" % e)
+    finally:
+        try:
+            client.disconnect()
+        except Exception as e:
+            print("Error2: %s" % e)
 
 # Callback function to handle messages
 def on_message(topic, msg):
@@ -30,35 +43,24 @@ def on_message(topic, msg):
     #iterations = rotation_steps
     factor = my_dick["angle"] if my_dick["mode"] == "angle" else int(FULL_ROTATION/360)
     data = ",".join([f"{(i)*factor}" for i in range(iterations)]) + ",shape,position,sensor_rotation,angle\n"
-    for _ in range(my_dick["runs"]):
-        for i in range(iterations):
+    for i in range(my_dick["runs"]):
+        for j in range(iterations):
             my_stepper.angle(my_dick["angle"]) if my_dick['mode'] == 'angle' else my_stepper.step(my_dick["angle"])
-            utime.sleep(0.5)
+            utime.sleep(0.1)
             dist = my_sensor.distance_mm()
-            utime.sleep(0.005)
             data += str(dist) + ','
-            utime.sleep(0.005)
         data += my_dick["shape"] + ','+ str(my_dick["position"]) + ',' + str(my_dick["sensor_rotation"]) + ',' + str(my_dick["angle"]) + '\n'
         my_stepper.angle(2.8125) if my_dick['shape'] != 'cylinder' else None
-        utime.sleep(1)
-    
+        if i%10 == 0 and i != 0: 
+            publish(data)
+            data = ""
+
     if my_dick["stop"]:
         should_stop_listening = True
         with open("continue", "w") as f:
             f.write("0")
-    
-    client = MQTTClient("ESP32DATA", SERVER, PORT)
-    try:
-        client.connect()
-        client.publish(b"data", data)
-        print(data)
-    except Exception as e:
-        print("Error1: %s" % e)
-    finally:
-        try:
-            client.disconnect()
-        except Exception as e:
-            print("Error2: %s" % e)
+    if not data: publish(data)
+    publish("stop")
     
 
 def main():
