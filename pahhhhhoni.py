422119afc94788f082b5cefd5288dfdaed7ae8b3
ricgoe <bluemchen_fenchel_0d@icloud.com>
2024-01-31 20:50:25 +0100

    misc

--- pahhhhhoni.py
+++ pahhhhhoni.py
@@ -72,7 +72,7 @@ def on_message(client: mqtt.Client, userdata, msg):
 
 if __name__ == '__main__':
     # sensor_rotation = 1 -> sensor is rotated vertically, sensor_rotation = 0 -> sensor is rotated horizontally
-    sender(password="jimmy4", angle=11.25, stop=False, runs=200, shape="prisma", mode = "angle", position = 4, sensor_rotation = 0)
+    sender(password="jimmy4", angle=11.25, stop=False, runs=200, shape="prisma", mode = "angle", position = 4, sensor_rotation = 0, multi = False)
     listener()
 
     
