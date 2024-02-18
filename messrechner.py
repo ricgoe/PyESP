a4a44a11a9fb4b887b3c71302aac803998e325a6
ricgoe <bluemchen_fenchel_0d@icloud.com>
2024-02-18 16:02:46 +0100

    live demo stuff

---
+++ messrechner.py
@@ -0,0 +1,135 @@
+import paho.mqtt.client as mqtt
+import json
+import matplotlib.pyplot as plt
+import pandas as pd
+from io import StringIO
+import os
+import numpy as np
+from scipy.signal import medfilt
+from scipy.fft import rfft, irfft, rfftfreq
+from keras.models import load_model
+
+# Configuration
+broker = "192.168.178.152"
+port = 1883
+
+def sender(**kwargs):
+    # Create a client instance
+    client = mqtt.Client()
+
+    try:
+        # Connect to the broker
+        client.connect(broker, port)
+
+        # Publish a message
+        client.publish("start", json.dumps(kwargs).encode('utf-8'))
+
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        # Disconnect the client
+        client.disconnect()
+         
+def listener():
+    client = mqtt.Client()
+    client.connect(broker, port)
+    client.subscribe("data")
+    client.on_message = on_message
+    client.loop_forever()
+
+
+def on_message(client: mqtt.Client, userdata, msg):
+    if msg.payload.decode('utf-8') == "stop":
+        client.loop_stop()
+        client.disconnect()
+        return
+    
+    
+    df = pd.read_csv(StringIO(msg.payload.decode('utf-8')))
+    df = df.iloc[:,:-4]
+    #print(df)
+    output_filename=f'scans_angle_demo.csv'
+    
+    #output_path = os.path.join(os.getcwd(), 'Logs_Multi')
+    output_path = os.path.join(os.getcwd(), 'DEMO')
+    output_filepath = os.path.join(output_path, output_filename)
+    df.to_csv(output_filepath, mode='a', header=not os.path.exists(output_filepath), index=False)
+    
+    median_window_size = 11
+    values_df = df
+    
+    data_filtered_df = pd.DataFrame()
+  
+    for i in range(len(df)):
+        data_filtered = medfilt(values_df.iloc[i], kernel_size=median_window_size)
+        data_filtered_fft_int = fft_filter(data_filtered)
+        data_filtered_df= pd.concat([data_filtered_df, pd.DataFrame(data_filtered_fft_int).T], axis=0, ignore_index=True)
+        
+        
+    data_filtered_df.columns = df.columns
+   
+
+    
+    
+    data_filtered_df_diff = data_filtered_df.diff(axis=1)
+    
+    print(data_filtered_df_diff)
+    
+    data_filtered_df_diff = data_filtered_df_diff.apply(lambda x: normalize_minus_one_to_one(x), axis=1)
+    data_filtered_df_diff = data_filtered_df_diff.apply(lambda x: x-np.mean(x), axis=1)
+    data_filtered_df_diff.fillna(0, inplace=True)
+    
+    model = load_model_demo()
+    X = data_filtered_df_diff
+    
+
+    predictions = model.predict(X)
+
+    print(predictions)
+    predicted_class = np.argmax(predictions, axis=1)
+    dict= {0: 'cube', 1: 'cylinder', 2: 'pentagon'}
+    import tkinter as tk
+    #tk window
+    window = tk.Tk()
+    window.title("Prediction")
+    window.geometry("1200x800")
+    label = tk.Label(window, text=f"{dict[predicted_class[0]]}")
+    label.pack(fill="both", expand=True)
+    window.mainloop()
+    print(predicted_class)
+    
+        
+
+
+def normalize_minus_one_to_one(array):
+    return 2 * ((array - np.min(array)) / (np.max(array) - np.min(array))) - 1
+
+
+def fft_filter(row, cutoff=0.07):
+    # FFT der Zeile
+    fft_coeffs = rfft(row)
+    fft_freqs = rfftfreq(len(row))
+    
+    # Filter: Entfernen hochfrequenter Komponenten
+    low_pass_filter = fft_freqs < cutoff
+    filtered_fft_coeffs = fft_coeffs * low_pass_filter
+    
+    # Rücktransformation in den Zeitbereich
+    filtered_row = irfft(filtered_fft_coeffs)
+    return filtered_row
+
+
+def load_model_demo():
+    model = load_model('general_object_recognition.h5')
+    return model
+
+
+if __name__ == '__main__':
+    # sensor_rotation = 1 -> sensor is rotated vertically, sensor_rotation = 0 -> sensor is rotated horizontally
+    sender(password="jimmy4", angle=4, stop=False, runs=1, shape="pentagon", mode = "step", position = 13, sensor_rotation = 1, multi = False)
+    listener()
+    
+    
+    
---
+++ mymqtt_demo.py
@@ -0,0 +1,94 @@
+# type: ignore
+import utime
+from umqtt.simple import MQTTClient
+from sensor import Sensor
+from stepper import Stepper
+import json
+
+# Configuration
+SERVER = "10.42.0.1"
+PORT = 1883
+TOPIC = b"start"
+with open("continue", "r") as f:
+    should_stop_listening = f.read() == "0"
+should_stop_listening = False # Debug
+my_sensor = Sensor(trigger_pin=32, echo_pin=33)
+my_stepper = Stepper(19, 18, 5, 17)
+
+FULL_ROTATION = Stepper.FULL_ROTATION
+
+def publish(datastr):
+    client = MQTTClient("ESP32DATA", SERVER, PORT)
+    try:
+        client.connect()
+        client.publish(b"data", datastr)
+        print(datastr)
+    except Exception as e:
+        print("Error1: %s" % e)
+    finally:
+        try:
+            client.disconnect()
+        except Exception as e:
+            print("Error2: %s" % e)
+
+# Callback function to handle messages
+def on_message(topic, msg):
+    global should_stop_listening
+    print("Topic: %s, Message: %s" % (topic, msg))
+    my_dick = json.loads(msg)
+    
+    if my_dick["password"] != "jimmy4":
+        return
+    iterations = int(360/my_dick["angle"]) if my_dick["mode"]=="angle" else int(FULL_ROTATION/my_dick["angle"])
+    #iterations = rotation_steps
+    factor = my_dick["angle"] if my_dick["mode"] == "angle" else int(FULL_ROTATION/360)
+    data = ",".join([f"{(i)*factor}" for i in range(iterations)]) + ",shape,position,sensor_rotation,angle\n"
+    for i in range(my_dick["runs"]):
+        for j in range(iterations):
+            my_stepper.angle(my_dick["angle"]) if my_dick['mode'] == 'angle' else my_stepper.step(my_dick["angle"])
+            utime.sleep(0.1)
+            dist_list = []
+            for k in range(1+(int(my_dick["multi"])*2)):
+                dist_list.append(my_sensor.distance_mm())
+                utime.sleep(0.03)        
+            data += str(sum(dist_list)/len(dist_list)) + ','
+        data += my_dick["shape"] + ','+ str(my_dick["position"]) + ',' + str(my_dick["sensor_rotation"]) + ',' + str(my_dick["angle"]) + '\n'
+        my_stepper.angle(2.8125) if my_dick['shape'] != 'cylinder' else None
+    
+    
+    if my_dick["stop"]:
+        should_stop_listening = True
+        with open("continue", "w") as f:
+            f.write("0")
+    
+    publish(data)
+    publish("stop")
+    
+
+def main():
+    global should_stop_listening
+    client = MQTTClient("ESP32", SERVER, port=PORT)
+    client.set_callback(on_message)
+
+    try:
+        client.connect()
+        print("Connected to %s:%s" % (SERVER, PORT))
+        client.subscribe(TOPIC)
+        print("Subscribed to %s" % TOPIC)
+
+        while not should_stop_listening:
+            # Check for new messages
+            client.check_msg()
+            utime.sleep(1)
+    
+    except Exception as e:
+        print("Error: %s" % e)
+    
+    finally:
+        try:
+            client.disconnect()
+        except Exception:
+            pass
+
+if __name__ == "__main__":
+    main()
